a2892bc136cb264e8bb335a878f88ca9fd2749b9
heaust <heaust.azure@gmail.com>
2022-06-08 19:06:28 +0530

    Side Panel headers added

--- src/components/SidePanel/SidePanel.tsx
+++ src/components/SidePanel/SidePanel.tsx
@@ -1,4 +1,5 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
+import { ViewportSidePanelTabs } from "../../enums";
 import { AllViewportSidePanelTabs } from "../../utils/constants";
 import GetIcon from "./Icons";
 import styles from "./SidePanel.module.css";
@@ -6,16 +7,34 @@ import styles from "./SidePanel.module.css";
 interface SidePanelProps {}
 
 const SidePanel: FunctionComponent<SidePanelProps> = () => {
+  const [selectedPropertyTab, setselectedPropertyTab] = useState(
+    ViewportSidePanelTabs.world
+  );
   return (
     <div className={`${styles.sidePanel}`}>
       <div className={`${styles.objectList}`}></div>
       <div className={`${styles.properties}`}>
         <div className={`${styles.iconTab}`}>
           {AllViewportSidePanelTabs.map((tab, index) => (
-            <div key={index} className={styles.iconWrapper}>{GetIcon(tab)}</div>
+            <div
+              onClick={() => setselectedPropertyTab(tab)}
+              key={index}
+              style={
+                selectedPropertyTab === tab ? { filter: "brightness(2)" } : {}
+              }
+              className={styles.iconWrapper}
+            >
+              {GetIcon(tab)}
+            </div>
           ))}
         </div>
-        <div className={`${styles.propertyTab}`}></div>
+        <div className={`${styles.propertyTab}`}>
+          <h2 className={`${styles.propertyTabHeader}`}>
+            {`${selectedPropertyTab}${
+              selectedPropertyTab.endsWith("s") ? "" : " Properties"
+            }`}
+          </h2>
+        </div>
       </div>
     </div>
   );
